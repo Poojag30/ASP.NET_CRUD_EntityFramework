9ed5e468113a85ab5260818096ac67b1baca4ffb
Poojagundami <104686422+Poojag30@users.noreply.github.com>
2024-03-09 17:34:18 +0530

    EF_CodeFirst

---
+++ EF_CodeFirst/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using EF_C1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EF_C1.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentContext db = new StudentContext();
+//private object students;
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.students.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ EF_CodeFirst/Models/student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EF_C1.Models
+{
+    public class student
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+    }
+}
