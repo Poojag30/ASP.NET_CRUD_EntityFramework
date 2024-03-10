e6e840313365e76e858c495692a2ff595b014e5a
Poojagundami <104686422+Poojag30@users.noreply.github.com>
2024-03-10 21:14:57 +0530

    EF_DB1

---
+++ EF_DB1/Controllers/studentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using entitydb1.context;
+namespace entitydb1.Controllers
+{
+    public class studentController : Controller
+    {
+        // GET: student
+        db_testEntities dbObj = new db_testEntities();
+       
+        public ActionResult Student()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddStudent(tbl_student model)
+        {
+            tbl_student obj = new tbl_student();
+            return View();
+        }
+        public ActionResult Student()
+        {
+            return View();
+        }
+        public ActionResult Student()
+        {
+            return View();
+        }
+        public ActionResult Student()
+        {
+            return View();
+        }
+        public ActionResult Student()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ EF_DB1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="entitydb1.MvcApplication" Language="C#" %>
